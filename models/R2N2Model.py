8410767b2d9f689e35b4cb72ca2ff3c54f6af483
Keven Wang <keven425@gmail.com>
2017-05-30 12:57:25 -0700

    added back iou objective

--- models/R2N2Model.py
+++ models/R2N2Model.py
@@ -35,9 +35,9 @@ class R2N2Model(Model):
                                  poses_batch=poses_batch,
                                  lr=lr,
                                  dropout_keep=self.config.dropout_keep)
-    _, loss, grad_norm, learning_rate, logits_pose_norm, grads_vars = sess.run(
-      [self.train_op, self.loss, self.grad_norm, self.learning_rate_placeholder, self.logits_pose_norm, self.grads_vars], feed_dict=feed)
-    return loss, grad_norm, learning_rate, logits_pose_norm, grads_vars
+    _, loss, grad_norm, learning_rate, logits_pose_norm, logits_label_norm, grads_vars = sess.run(
+      [self.train_op, self.loss, self.grad_norm, self.learning_rate_placeholder, self.logits_pose_norm, self.logits_label_norm, self.grads_vars], feed_dict=feed)
+    return loss, grad_norm, learning_rate, logits_pose_norm, logits_label_norm, grads_vars
 
   def evaluate_on_batch(self, sess, input_batch, labels_batch, poses_batch):
     feed = self.create_feed_dict(is_training=False,
@@ -45,7 +45,7 @@ class R2N2Model(Model):
                                  labels_batch=labels_batch,
                                  poses_batch=poses_batch)
     pred, loss = sess.run([self.pred, self.loss], feed_dict=feed)  # pick the class that has highest probability
-    delta_az, delta_el, delta_di = pred
+    delta_az, delta_el, delta_di, vox_pred = pred
     poses_label = poses_batch[:, 1:] - poses_batch[:, :-1]
     delta_az_label = poses_label[:, :, 0]
     delta_el_label = poses_label[:, :, 1]
@@ -53,7 +53,10 @@ class R2N2Model(Model):
     az_rmse = np.mean(np.linalg.norm(delta_az_label - delta_az, axis=-1))
     el_rmse = np.mean(np.linalg.norm(delta_el_label - delta_el, axis=-1))
     di_rmse = np.mean(np.linalg.norm(delta_di_label - delta_di, axis=-1))
-    return az_rmse, el_rmse, di_rmse
+
+    thresh = self.config.TEST.VOXEL_THRESH[0]
+    iou = lib.voxel.evaluate_voxel_prediction(vox_pred, labels_batch, thresh)
+    return az_rmse, el_rmse, di_rmse, iou
 
   def add_placeholders(self):
     self.is_training_placeholder = tf.placeholder(tf.bool, shape=())
@@ -154,14 +157,6 @@ class R2N2Model(Model):
       fc = tf.reshape(fc, shape=(-1, self.config.CONST.N_VIEWS, n_fc_outputs))
       fc = tf.Print(fc, [tf.reduce_min(fc), tf.reduce_max(fc), fc], message="fc")
 
-      # 3D GRU
-      # grid_state_size = (4, 4, 4, 128)
-      # cell = GRU3dCell(fc.shape[-1].value, grid_state_size)
-      # states, h = tf.nn.dynamic_rnn(cell, fc, dtype=tf.float32)
-      # shape = [-1] + list(grid_state_size)
-      # h = tf.reshape(h, shape=shape) # reshape back to 3d
-      # h = tf.Print(h, [tf.reduce_min(h), tf.reduce_max(h), h], message="3D GRU output")
-
       # predict pose delta
       states_concat = tf.concat([fc[:, 1:], fc[:, :-1]], axis=-1)
       states_concat_size = states_concat.get_shape()[-1].value
@@ -181,14 +176,73 @@ class R2N2Model(Model):
       delta_el = tf.Print(delta_el, [tf.reduce_min(delta_el), tf.reduce_max(delta_el), tf.reduce_mean(delta_el), delta_el], message="delta_el")
       delta_di = tf.Print(delta_di, [tf.reduce_min(delta_di), tf.reduce_max(delta_di), tf.reduce_mean(delta_di), delta_di], message="delta_di")
 
-    return delta_az, delta_el, delta_di
+      # 3D GRU
+      grid_state_size = (4, 4, 4, 128)
+      cell = GRU3dCell(fc.shape[-1].value, grid_state_size)
+      states, h = tf.nn.dynamic_rnn(cell, fc, dtype=tf.float32)
+      shape = [-1] + list(grid_state_size)
+      h = tf.reshape(h, shape=shape) # reshape back to 3d
+      h = tf.Print(h, [tf.reduce_min(h), tf.reduce_max(h), h], message="3D GRU output")
+
+      # deconvolutional layers
+      # 1st deconv layer
+      h = models.unpool_3d.unpool_3d_zero_filled(h)
+      deconv11 = tf.layers.conv3d(h, filters=128, kernel_size=[3, 3, 3], strides=(1, 1, 1), padding='same',
+                                  activation=tf.nn.relu, use_bias=False, name="deconv11", reuse=False)
+      deconv12 = tf.layers.conv3d(deconv11, filters=128, kernel_size=[3, 3, 3], strides=(1, 1, 1), padding='same',
+                                  activation=tf.nn.relu, use_bias=False, name="deconv12", reuse=False)
+      deconv1_res = tf.layers.conv3d(h, filters=128, kernel_size=[1, 1, 1], strides=(1, 1, 1), padding='same',
+                                     activation=tf.nn.relu, use_bias=False, name="deconv1_res", reuse=False)
+      deconv1 = deconv12 + deconv1_res
+
+      # 2nd deconv layer
+      deconv1 = models.unpool_3d.unpool_3d_zero_filled(deconv1)
+      deconv21 = tf.layers.conv3d(deconv1, filters=128, kernel_size=[3, 3, 3], strides=(1, 1, 1), padding='same',
+                                  activation=tf.nn.relu, use_bias=False, name="deconv21", reuse=False)
+      deconv22 = tf.layers.conv3d(deconv21, filters=128, kernel_size=[3, 3, 3], strides=(1, 1, 1), padding='same',
+                                  activation=tf.nn.relu, use_bias=False, name="deconv22", reuse=False)
+      deconv2_res = tf.layers.conv3d(deconv1, filters=128, kernel_size=[1, 1, 1], strides=(1, 1, 1), padding='same',
+                                     activation=tf.nn.relu, use_bias=False, name="deconv2_res", reuse=False)
+      deconv2 = deconv22 + deconv2_res
+
+      # 3rd deconv layer
+      deconv2 = models.unpool_3d.unpool_3d_zero_filled(deconv2)
+      deconv31 = tf.layers.conv3d(deconv2, filters=64, kernel_size=[3, 3, 3], strides=(1, 1, 1), padding='same',
+                                  activation=tf.nn.relu, use_bias=False, name="deconv31", reuse=False)
+      deconv32 = tf.layers.conv3d(deconv31, filters=64, kernel_size=[3, 3, 3], strides=(1, 1, 1), padding='same',
+                                  activation=tf.nn.relu, use_bias=False, name="deconv32", reuse=False)
+      deconv3_res = tf.layers.conv3d(deconv2, filters=64, kernel_size=[1, 1, 1], strides=(1, 1, 1), padding='same',
+                                     activation=tf.nn.relu, use_bias=False, name="deconv3_res", reuse=False)
+      deconv3 = deconv32 + deconv3_res
+
+      # 4th deconv layer
+      deconv41 = tf.layers.conv3d(deconv3, filters=32, kernel_size=[3, 3, 3], strides=(1, 1, 1), padding='same',
+                                  activation=tf.nn.relu, use_bias=False, name="deconv41", reuse=False)
+      deconv42 = tf.layers.conv3d(deconv41, filters=32, kernel_size=[3, 3, 3], strides=(1, 1, 1), padding='same',
+                                  activation=tf.nn.relu, use_bias=False, name="deconv42", reuse=False)
+      deconv4_res = tf.layers.conv3d(deconv3, filters=32, kernel_size=[1, 1, 1], strides=(1, 1, 1), padding='same',
+                                     activation=tf.nn.relu, use_bias=False, name="deconv4_res", reuse=False)
+      deconv4 = deconv42 + deconv4_res
+
+      # final deconv layer
+      deconv5 = tf.layers.conv3d(deconv4, filters=2, kernel_size=[3, 3, 3], strides=(1, 1, 1), padding='same',
+                                 activation=None, use_bias=False, name="deconv4", reuse=False)
+      deconv5 = tf.Print(deconv5, [tf.reduce_min(deconv5), tf.reduce_max(deconv5), deconv5], message="deconv5")
+
+    return delta_az, delta_el, delta_di, deconv5
 
   def add_prediction_op(self, logits):
     return logits
 
   def add_loss_op(self, logits):
-    delta_az, delta_el, delta_di = logits
-    self.logits_pose_norm = tf.sqrt(tf.reduce_mean(tf.square(logits)))
+    delta_az, delta_el, delta_di, deconv = logits
+    self.logits_pose_norm = tf.sqrt(tf.reduce_mean(tf.square([delta_az, delta_el, delta_di])))
+    self.logits_label_norm = tf.sqrt(tf.reduce_mean(tf.square(deconv)))
+    self.labels_placeholder = tf.Print(self.labels_placeholder, [self.labels_placeholder], message="labels")
+
+    cross_entropy_label = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=deconv, labels=self.labels_placeholder)
+    loss_label = tf.reduce_mean(cross_entropy_label)
+    loss_label = tf.Print(loss_label, [loss_label], message="loss_label")
 
     delta_poses_label = self.poses_placeholder[:,1:] - self.poses_placeholder[:,:-1]
     delta_az_label = delta_poses_label[:, :, 0]
@@ -205,7 +259,7 @@ class R2N2Model(Model):
     rmse_el = tf.Print(rmse_el, [rmse_el], message="rmse_el")
     rmse_di = tf.Print(rmse_di, [rmse_di], message="rmse_di")
 
-    loss = tf.reduce_mean([rmse_az, rmse_el, rmse_di])
+    loss = tf.reduce_mean([rmse_az, rmse_el, rmse_di]) + loss_label
     return loss
 
   def add_training_op(self, loss):
